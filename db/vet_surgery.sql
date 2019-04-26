28b48c87da3052a848b3ca8cd921f09437f122df
Jonathan Tomaney <jon.tomaney@gmail.com>
2019-04-26 13:42:34 +0100

    tables created

---
+++ db/vet_surgery.sql
@@ -0,0 +1,17 @@
+DROP TABLE pets;
+DROP TABLE vets;
+
+CREATE TABLE vets (
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE pets (
+  id SERIAL8 PRIMARY KEY,
+  vet_id INT8 REFERENCES vets(id) ON DELETE CASCADE,
+  name VARCHAR(255),
+  date_of_birth VARCHAR(255),
+  species VARCHAR(255),
+  owner_contact_info VARCHAR(255),
+  treatment_notes VARCHAR(510)
+);
