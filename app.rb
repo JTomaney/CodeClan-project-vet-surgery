14bff27feab3fd5eae86118b2c0cef8b3a84f348
Jonathan Tomaney <jon.tomaney@gmail.com>
2019-04-29 08:07:41 +0100

    main index working

---
+++ app.rb
@@ -0,0 +1,8 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('controllers/pet_controller.rb')
+require_relative('controllers/vet_controller.rb')
+
+get '/' do
+  erb(:index)
+end
