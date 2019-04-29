afcac04c121d8993911683168ad662c90df4f2a2
Jonathan Tomaney <jon.tomaney@gmail.com>
2019-04-29 14:02:35 +0100

    basic style and more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@ vet1 = Vet.new({'name' => 'Dr. Jones'})
 vet1.save()
 vet2 = Vet.new({'name' => 'Dr. Crusher'})
 vet2.save()
+vet3 = Vet.new({'name' => 'Dr. Octopus'})
+vet3.save()
 
 pet1 = Pet.new({'name' => 'Indi',
   'date_of_birth' => '22/02/1994',
@@ -16,7 +18,20 @@ pet1 = Pet.new({'name' => 'Indi',
   'treatment_notes' => 'prescribed worming/flea tablets',
   'vet_id' => vet1.id()})
 pet1.save()
-
+pet2 = Pet.new({'name' => 'Chewie',
+  'date_of_birth' => '30/10/2016',
+  'species' => 'dog',
+  'owner_contact_info' => 'Mari, 01337830994',
+  'treatment_notes' => 'prescribed steroid cream for left ear',
+  'vet_id' => vet2.id()})
+pet2.save()
+pet3 = Pet.new({'name' => 'Kurt',
+  'date_of_birth' => '06/08/2005',
+  'species' => 'gerbil',
+  'owner_contact_info' => 'Mat, 01923291248',
+  'treatment_notes' => 'Routine checkup, no treatment',
+  'vet_id' => vet3.id()})
+pet3.save()
 
 
 binding.pry()
