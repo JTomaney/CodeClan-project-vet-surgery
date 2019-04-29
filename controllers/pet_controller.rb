c5e9245165c1745fb60626be83daabfee81c1257
Jonathan Tomaney <jon.tomaney@gmail.com>
2019-04-29 13:40:05 +0100

    improved navigation

--- controllers/pet_controller.rb
+++ controllers/pet_controller.rb
@@ -2,39 +2,39 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/pet.rb')
 also_reload('../models/*')
-
+# index
 get '/pets' do
   @pets = Pet.all()
   erb(:"pets/index")
 end
-
+#new
 get '/pets/new' do
   @vets = Vet.all()
   erb(:"pets/new")
 end
-
+#create
 post '/pets' do
   Pet.new(params).save()
   redirect to '/pets'
 end
-
+#show
 get '/pets/:id' do
   @pet = Pet.find(params['id'].to_i)
   erb(:"pets/show")
 end
-
+#edit
 get '/pets/:id/edit' do
   @pet = Pet.find(params['id'].to_i)
   @vets = Vet.all()
   erb(:"pets/edit")
 end
-
+#update
 post '/pets/:id' do
   pet = Pet.new(params)
   pet.update()
   redirect to '/pets'
 end
-
+#delete
 post '/pets/:id/delete' do
   pet = Pet.find(params['id'].to_i)
   pet.delete()
--- controllers/vet_controller.rb
+++ controllers/vet_controller.rb
@@ -2,37 +2,37 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/vet.rb')
 also_reload('../models/*')
-
+# index
 get '/vets' do
   @vets = Vet.all()
   erb(:"vets/index")
 end
-
+# new
 get '/vets/new' do
   erb(:"vets/new")
 end
-
+# create
 post '/vets' do
   Vet.new(params).save()
-  erb(:"vets/index")
+  redirect to '/vets'
 end
-
+# show
 get '/vets/:id' do
   @vet = Vet.find(params['id'].to_i)
   erb(:"vets/show")
 end
-
+# edit
 get '/vets/:id/edit' do
   @vet = Vet.find(params['id'].to_i)
   erb(:"vets/edit")
 end
-
+# update
 post '/vets/:id' do
   vet = Vet.new(params)
   vet.update()
   redirect to '/vets'
 end
-
+# delete
 post '/vets/:id/delete' do
   vet = Vet.find(params['id'].to_i)
   vet.delete()
