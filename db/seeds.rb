f819ad8ff111adb9bfd5079e5049543f1fa31668
Jonathan Tomaney <jon.tomaney@gmail.com>
2019-04-29 07:47:38 +0100

    Pet initialize and save

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,5 +9,13 @@ vet1.save()
 vet2 = Vet.new({'name' => 'Dr. Crusher'})
 vet2.save()
 
+pet1 = Pet.new({'name' => 'Indi',
+  'date_of_birth' => '22/02/1994',
+  'species' => 'cat',
+  'owner_contact_info' => 'Jon, 07734975635',
+  'treatment_notes' => 'prescribed worming/flea tablets',
+  'vet_id' => vet1.id()})
+pet1.save()
+
 binding.pry()
 nil
--- models/pet.rb
+++ models/pet.rb
@@ -1,8 +1,28 @@
+require_relative('../db/sql_runner.rb')
+
 class Pet
 
-def initialize(options)
+  attr_reader :id
+  attr_accessor :name, :date_of_birth, :species, :owner_contact_info, :treatment_notes, :vet_id
 
-end
+  def initialize(options)
+    @id = options["id"].to_i if options["id"]
+    @name = options["name"]
+    @date_of_birth = options["date_of_birth"]
+    @species = options["species"]
+    @owner_contact_info = options["owner_contact_info"]
+    @treatment_notes = options["treatment_notes"]
+    @vet_id = options["vet_id"].to_i if options["vet_id"]
+  end
+
+  def save()
+    sql = 'INSERT INTO pets
+      (name, date_of_birth, species, owner_contact_info, treatment_notes, vet_id) VALUES ($1, $2, $3, $4, $5, $6)
+      RETURNING id;'
+    values = [@name, @date_of_birth, @species, @owner_contact_info, @treatment_notes, @vet_id]
+    results = SqlRunner.run(sql, values)
+    @id = results[0]['id'].to_i
+  end
 
 
 end
