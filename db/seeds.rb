cf8c0fc3cdd30a5b8e66962a909fbabc95f940c4
Jonathan Tomaney <jon.tomaney@gmail.com>
2019-04-30 11:30:02 +0100

    new seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,10 +13,20 @@ client1 = Client.new({'first_name'=> 'Jon',
                       'contact_number' => '01923291248',
                       'email' => 'jt@mail.com'})
 client1.save()
+client2 = Client.new({'first_name'=> 'Bill',
+                      'last_name' => 'Billson',
+                      'contact_number' => '7265024857',
+                      'email' => 'bb@mail.com'})
+client2.save()
+client3 = Client.new({'first_name'=> 'Harold',
+                      'last_name' => 'Gregg',
+                      'contact_number' => '07364983729',
+                      'email' => 'hg@mail.com'})
+client3.save()
 
 vet1 = Vet.new({'name' => 'Dr. Jones'})
 vet1.save()
-vet2 = Vet.new({'name' => 'Dr. Crusher'})
+vet2 = Vet.new({'name' => 'Dr. Bacta'})
 vet2.save()
 vet3 = Vet.new({'name' => 'Dr. Octopus'})
 vet3.save()
@@ -38,10 +48,17 @@ pet2.save()
 pet3 = Pet.new({'name' => 'Kurt',
   'date_of_birth' => '06/08/2005',
   'species' => 'gerbil',
-  'client_id' => client1.id(),
+  'client_id' => client2.id(),
   'treatment_notes' => 'Routine checkup, no treatment',
   'vet_id' => vet3.id()})
 pet3.save()
+pet4 = Pet.new({'name' => 'Muncher',
+  'date_of_birth' => '22/04/2009',
+  'species' => 'rabbit',
+  'client_id' => client3.id(),
+  'treatment_notes' => 'Chewed through a live wire, pronounced dead',
+  'vet_id' => vet3.id()})
+pet4.save()
 
 
 
